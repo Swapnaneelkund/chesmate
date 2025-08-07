a812acb1108d2c4996bd044b8dbc7b6448694880
swapnaneel <swapnaneelkundu2@gmail.com>
2025-08-07 14:50:47 +0530

    uploaded fixed version

---
+++ app.js
@@ -0,0 +1,82 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { Server as SocketServer } from "socket.io";
+import { inject } from "@vercel/analytics";
+
+import http from "http";
+import { Chess } from "chess.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
+inject();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+const chess = new Chess();
+let players = {};
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/",(req,res)=>{
+	res.render("front")
+})
+app.get("/game", (req, res) => {
+	res.render("index", { title: "Chess Game" });
+});
+
+io.on("connection", (uniquesocket) => {
+	console.log("connected");
+		uniquesocket.emit("boardState", chess.fen());
+	if (!players.white) {
+		players.white = uniquesocket.id;
+		uniquesocket.emit("playerRole", "w");
+	} else if (!players.black) {
+		players.black = uniquesocket.id;
+		uniquesocket.emit("playerRole", "b");
+	} else {
+		uniquesocket.emit("spectatorRole");
+	}
+	uniquesocket.on("disconnect", () => {
+		if (uniquesocket.id === players.white) {
+			delete players.white;
+		} else if (uniquesocket.id === players.black) {
+			delete players.black;
+		}
+	});
+	uniquesocket.on("move", (move) => {
+		try {
+			if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+			if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+			const result = chess.move(move);
+
+			if (result) {
+				io.emit("move", move);
+				io.emit("boardState", chess.fen());
+			} else {
+				console.log("Invalid move:", move);
+				uniquesocket.emit("invalidMove", move);
+			}
+		} catch (err) {
+			console.log(err);
+			uniquesocket.emit("invalidMove", move);
+		}
+	});
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Internal Server Error");
+});
+const port =process.env.PORT || 3000;
+server.listen(port,"0.0.0.0",() => {
+	console.log("app running");
+});
